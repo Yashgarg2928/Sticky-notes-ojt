505949e6d9b0c0078e3d7826715845bc976ce519
Madhvan <deepaksharma@Madhvan.local>
2025-12-02 09:44:50 +0530

    dark mode

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let attachAudio = document.querySelector(".attach-audio")
 let atatchVideo = document.querySelector(".attach-video")
 let deleteNote = document.querySelector(".delete-note")
 let workingArea = document.querySelector(".working")
+let themebtn = document.querySelector(".themebtn")
 let stickyNote;
 
 function makeNote(classname, mediaType){
@@ -167,3 +168,47 @@ deleteNote.addEventListener("click", function () {
         deleteStickyNote.remove();
     });
 });
+
+themebtn.addEventListener("click",function(){
+    let root = document.documentElement;
+    const themelogo = themebtn.querySelector("svg")
+    const moon = "M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+
+    const sun = "M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+
+    switch (getComputedStyle(root).getPropertyValue("--color1")){
+        case "#363A42":
+            let props = {
+                "--color1": "#B2AAC1",
+                "--color2": "#DCDBDD",
+                "--color3": "#C7E9EE",
+                "--color4": "#174579",
+                "--boardcolor": "#DCDBDD",
+                "--colcolor": "#000000"
+            }
+            for(const prop in props){
+                root.style.setProperty(prop,props[prop])
+            }
+            
+            
+            themelogo.querySelector("path").setAttribute("d",sun)
+            
+            break;
+        case "#B2AAC1":
+            let darkprops = {
+                "--color1": "#363A42",
+                "--color2": "#B2A7BC",
+                "--color3": "#415563",
+                "--color4": "#F0F5F7",
+                "--boardcolor": "#363A42",
+                "--colcolor": "#727880"
+            }
+            for(const darkprop in darkprops){
+                root.style.setProperty(darkprop,darkprops[darkprop])
+            }
+            themelogo.querySelector("path").setAttribute("d",moon)
+
+            break;
+    }
+    
+})
